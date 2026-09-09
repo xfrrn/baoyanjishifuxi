d5cae104285e2e13261ab6c339afb50d2ba31823
xiaoxin <1276179128@qq.com>
2026-09-10 00:53:27 +0800

    feat: 结束第二题

--- cpp-exam-review/problems/002-score-statistics/main.cpp
+++ cpp-exam-review/problems/002-score-statistics/main.cpp
@@ -1,7 +1,25 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int main() {
+int main(){
+    int n,k;
+    cin>>n;
+    cin.get();
+    cin>>k;
+    cin.get();
 
-    return 0;
+    int all_score=0;
+    int dabiao=0;
+
+    for(int s=0;s<n;s++){
+        int score;
+        cin>>score;
+        if(score>=k){
+            all_score+=score;
+            dabiao+=1;
+        }
+    }
+    
+    cout<<dabiao<<" "<<all_score;
+    
 }
