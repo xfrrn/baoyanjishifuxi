6ec1f583c2e5054da30fc6c2936a78146d47e4dd
xiaoxin <1276179128@qq.com>
2026-09-10 12:18:55 +0800

    feat: 解开第七题

--- cpp-exam-review/problems/007-stock-records/main.cpp
+++ cpp-exam-review/problems/007-stock-records/main.cpp
@@ -1,7 +1,33 @@
-#include <bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
 
-int main() {
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int op[1000][2];
 
-    return 0;
+    int falses=0;
+
+    for(int i=0;i<n;i++){
+        cin>>op[i][0];
+        cin>>op[i][1];
+    }
+
+    for(int i=0;i<n;i++){
+        if(op[i][0]==1){
+            m+=op[i][1];
+        }
+         if(op[i][0]==2){
+            if(m>=op[i][1]){
+            m-=op[i][1];}
+            else{
+                falses++;
+            }
+        }
+
+
+    }
+    cout<<m<<' '<<falses;
+
 }
