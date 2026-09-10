44bf92c96ed9c1549c195572f8646d0e752d379f
xiaoxin <1276179128@qq.com>
2026-09-10 11:37:25 +0800

    feat: 解开第五题

--- cpp-exam-review/problems/005-height-order/main.cpp
+++ cpp-exam-review/problems/005-height-order/main.cpp
@@ -1,7 +1,20 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
 using namespace std;
 
-int main() {
-
-    return 0;
+int main()
+{
+    int n;
+    int hig[1000];
+    cin>>n;
+    for(int s=0;s<n;s++){
+        cin>>hig[s];
+    }
+    sort(hig,hig+n);
+    for(int s=0;s<n;s++){
+        cout<<hig[s];
+        if(s!=n-1){
+            cout<<' ';
+        }
+    }
 }
