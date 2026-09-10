315ed7f77ab455b9da8b444b6c3c2f3c46ee33d8
xiaoxin <1276179128@qq.com>
2026-09-10 11:27:31 +0800

    feat: 解开第四题

--- cpp-exam-review/problems/004-digit-count/main.cpp
+++ cpp-exam-review/problems/004-digit-count/main.cpp
@@ -1,7 +1,31 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+
 using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cin.get();
 
-int main() {
+    for(int o=0;o<n;o++){
+        string line;
+        getline(cin,line);
+        int num=line.size();
+        int num_size=0;
+        for(int oo=0;oo<num;oo++){
 
-    return 0;
+            if(line[oo]=='0'){num_size++;}
+            if(line[oo]=='1'){num_size++;}
+            if(line[oo]=='2'){num_size++;}
+            if(line[oo]=='3'){num_size++;}
+            if(line[oo]=='4'){num_size++;}
+            if(line[oo]=='5'){num_size++;}
+            if(line[oo]=='6'){num_size++;}
+            if(line[oo]=='7'){num_size++;}
+            if(line[oo]=='8'){num_size++;}
+            if(line[oo]=='9'){num_size++;}
+            
+        }
+        cout<<num_size<<'\n';
+    }
 }
