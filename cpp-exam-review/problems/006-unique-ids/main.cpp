773734b2a4ea7ad3279f5f8080c5d67afa0c19e0
xiaoxin <1276179128@qq.com>
2026-09-10 11:59:40 +0800

    feat: 解开第六题

--- cpp-exam-review/problems/006-unique-ids/main.cpp
+++ cpp-exam-review/problems/006-unique-ids/main.cpp
@@ -1,6 +1,31 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main(){
-    
+    int n;
+    cin>>n;
+    int number[1000];
+    for(int i=0;i<n;i++){
+        cin>>number[i];
+    }
+    sort(number,number+n);
+
+    int diff=0;
+    int diffs[1000];
+
+    for(int i=0;i<n;){
+        while(number[i]==number[i+1]){
+            i++;
+        }
+        
+        diffs[diff]=number[i];
+        diff++;
+        i++;
+
+    }
+    cout<<diff<<'\n';
+    for(int i=0;i<diff;i++){
+        cout<<diffs[i]<<' ';
+    }
 }
